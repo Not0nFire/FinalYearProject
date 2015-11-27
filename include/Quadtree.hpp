aabb655bac5e76b63f6d6c154e19a296039424da
Alex Meuer <alexmeuer@gmail.com>
2015-11-27 17:40:38 +0000

    Wrote Quadtree draw method and began subdivide method. Changed class to use smart pointers. Child nodes are stored in a vector now instead of array. Fixed some glaring mistakes.

--- include/Quadtree.hpp
+++ include/Quadtree.hpp
@@ -1,7 +1,7 @@
 #ifndef _QUADTREE_H
 #define _QUADTREE_H
 
-#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics.hpp>
 #include <memory>
 #include <functional>
 #include <vector>
@@ -11,55 +11,86 @@ template<class T>
 class Quadtree {
 private:
 	typedef std::shared_ptr<T*> T_ptr;
-	enum Node {
+	typedef std::unique_ptr<Quadtree<T>> Node;
+	
+	enum Nodes {
 		NW = 0,
-		NE = 1,
-		SW = 2,
-		SE = 3,
-		NODE_COUNT = 4
+		NE,
+		SW,
+		SE,
+		NODE_COUNT
 	};
 
 	bool mIsLeaf;
-	Quadtree<T_ptr>* mNodes;
+
 	vector<T_ptr> mItems;
 	sf::IntRect mBounds;
 
+	vector<Node> mNodes;
+
 	bool contains(const T_ptr item, bool* whollyContained) const;
 
-	static const unsigned int MAX_ITEMS;
+#ifdef _DEBUG
+	sf::RectangleShape mShape;
+#endif	//_DEBUG
 
 public:
 	Quadtree<T>(int left, int top, int width, int height);
 	Quadtree<T>(sf::IntRect const &bounds);
 	~Quadtree<T>();
 
+	//Inserts an item into the tree at the deepest node that wholly contains the object
 	void insert(T_ptr item);
+
+	//Clears all items from this node and all child nodes
 	void clear();
 
-	vector<T*> query(float x, float y);
+	//Recursively gets all items in this node and in all child nodes.
+	vector<T_ptr>&& getItems() const;
+
+	vector<T_ptr>&& query(float x, float y);
 	bool contains(int x, int y) const;
 
-	void subdivide(vector<T_ptr> items, std::function<bool(T_ptr)> predicate);
+	//Subdivides this node and all child nodes upon a given predicate.
+	void subdivide(std::function<bool(Quadtree* node)> predicate);
+
+	sf::IntRect getBounds() const;
+
+#ifdef _DEBUG
+	void draw(sf::RenderTarget &target);
+#endif	//_DEBUG
 };
 
 template <class T>
 Quadtree<T>::Quadtree(int left, int top, int width, int height) :
 mIsLeaf(true),
-mNodes(nullptr),
 mItems(),
 mBounds(left, top, width, height)
 {
-	mItems.reserve(Node::NODE_COUNT);
+	mNodes.reserve(NODE_COUNT);
+#ifdef _DEBUG
+	mShape = sf::RectangleShape(sf::Vector2f(mBounds.width, mBounds.height));
+	mShape.setPosition(mBounds.left, mBounds.top);
+	mShape.setFillColor(sf::Color::Transparent);
+	mShape.setOutlineColor(sf::Color::Magenta);
+	mShape.setOutlineThickness(1.f);
+#endif	//_DEBUG
 }
 
 template <class T>
 Quadtree<T>::Quadtree(sf::IntRect const &bounds) :
 mIsLeaf(true),
-mNodes(nullptr),
 mItems(),
 mBounds(bounds)
 {
-	mItems.reserve(Node::NODE_COUNT);
+	mNodes.reserve(NODE_COUNT);
+#ifdef _DEBUG
+	mShape = sf::RectangleShape(sf::Vector2f(mBounds.width, mBounds.height));
+	mShape.setPosition(mBounds.left, mBounds.top);
+	mShape.setFillColor(sf::Color::Transparent);
+	mShape.setOutlineColor(sf::Color::Magenta);
+	mShape.setOutlineThickness(1.f);
+#endif	//_DEBUG
 }
 
 template <class T>
@@ -70,9 +101,9 @@ template <class T>
 void Quadtree<T>::insert(T_ptr item) {
 	if (mIsLeaf) {
 		mItems.push_back(item);
+	} else {
+		//give item to a child node
 	}
-
-
 }
 
 template <class T>
@@ -80,26 +111,37 @@ void Quadtree<T>::clear() {
 	mItems.clear();
 
 	if (!mIsLeaf) {
-		for (int i = 0; i < Node::NODE_COUNT; ++i) {
-			mNodes[i].clear();
+		for (Node n : mNodes) {
+			n->clear();
 		}
 	}
 }
 
 template <class T>
-vector<T*> Quadtree<T>::query(float x, float y) {
+vector<typename Quadtree<T>::T_ptr>&& Quadtree<T>::getItems() const {
+	vector<T_ptr> returnedItems = vector<T_ptr>(mItems);
+
+	for (Node n : mNodes) {
+		returnedItems.emplace_back(n->getItems());
+	}
+
+	return std::move(returnedItems);
+}
+
+template <class T>
+vector<typename Quadtree<T>::T_ptr>&& Quadtree<T>::query(float x, float y) {
 	_ASSERT(mBounds.contains(x, y));
 
 	vector<T*> returnedItems(mItems);
 
-	for (int i = 0; i < Node::NODE_COUNT; ++i) {
-		if (mNodes[i].contains(x, y)) {
-			returnedItems.push_back(mNodes[i].query(x, y));
+	for (Node n : mNodes) {
+		if (n->contains(x, y)) {
+			returnedItems.push_back(n->query(x, y));
 			break;	//Breaking because only one of the child nodes will contain the x,y point
 		}
 	}
 
-	return returnedItems;
+	return std::move(returnedItems);
 }
 
 template <class T>
@@ -130,7 +172,45 @@ bool Quadtree<T>::contains(const T_ptr item, bool* whollyContained) const {
 }
 
 template <class T>
-void Quadtree<T>::subdivide(vector<T_ptr> items, std::function<bool(T_ptr)> predicate) {
-	throw "Not Implemented -- Quadtree<T>::subdivide(...)";
+void Quadtree<T>::subdivide(std::function<bool(Quadtree* node)> predicate) {
+
+	if (mIsLeaf && predicate(this)) {
+		mIsLeaf = false;
+
+		//Create child nodes
+		unsigned int halfWidth = mBounds.width / 2;
+		unsigned int halfHeight = mBounds.height / 2;
+		mNodes[NW] = new Quadtree<T>(mBounds.left, mBounds.top, halfWidth, halfHeight);
+		mNodes[NE] = new Quadtree<T>(mBounds.left + halfWidth, mBounds.top, halfWidth, halfHeight);
+		mNodes[SW] = new Quadtree<T>(mBounds.left, mBounds.top + halfHeight, halfWidth, halfHeight);
+		mNodes[SE] = new Quadtree<T>(mBounds.left + halfWidth, mBounds.top + halfHeight, halfWidth, halfHeight);
+
+		for (T_ptr item : mItems) {
+			for (Node n : mNodes) {
+				
+				//Move items to child nodes (only if wholly contained??).
+				//(if we give this node the item, break so that we don't try to give it to another node)
+
+			}//end for(each node)
+		}//end for(each item)
+	}//end if(leaf && predicate)
 }
-#endif
+
+template <class T>
+sf::IntRect Quadtree<T>::getBounds() const {
+	return mBounds;
+}
+
+#ifdef _DEBUG
+template <class T>
+void Quadtree<T>::draw(sf::RenderTarget& target) {
+	if (mIsLeaf) {
+		target.draw(mShape);
+	} else {
+		for (Node n : mNodes) {
+			n->draw(target);
+		}
+	}
+}
+#endif	//_DEBUG
+#endif	//include guard
