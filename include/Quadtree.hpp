7826d5346413adcbac135ecfff1ad18a3c0370b9
Alex Meuer <alexmeuer@gmail.com>
2015-11-30 15:55:52 +0000

    Greatly simplified QuadTree. Was trying to accomplish too much without proper planning. Reduced class to bare essentials.

--- include/Quadtree.hpp
+++ include/Quadtree.hpp
@@ -5,212 +5,115 @@
 #include <memory>
 #include <functional>
 #include <vector>
+#include <sstream>
 using std::vector;
 
-template<class T>
 class Quadtree {
 private:
-	typedef std::shared_ptr<T*> T_ptr;
-	typedef std::unique_ptr<Quadtree<T>> Node;
-	
-	enum Nodes {
-		NW = 0,
-		NE,
-		SW,
-		SE,
-		NODE_COUNT
-	};
+	sf::FloatRect mBounds;
+	unsigned char mData;
+	unsigned int mLevel;
 
-	bool mIsLeaf;
-
-	vector<T_ptr> mItems;
-	sf::IntRect mBounds;
-
-	vector<Node> mNodes;
-
-	bool contains(const T_ptr item, bool* whollyContained) const;
-
-#ifdef _DEBUG
-	sf::RectangleShape mShape;
-#endif	//_DEBUG
+	Quadtree *mParent;
+	Quadtree *NW, *NE, *SW, *SE;	//child nodes
 
 public:
-	Quadtree<T>(int left, int top, int width, int height);
-	Quadtree<T>(sf::IntRect const &bounds);
-	~Quadtree<T>();
+	Quadtree(float x, float y, float width, float height, Quadtree* parent = nullptr);
 
-	//Inserts an item into the tree at the deepest node that wholly contains the object
-	void insert(T_ptr item);
+	~Quadtree();
 
-	//Clears all items from this node and all child nodes
-	void clear();
+	unsigned char getData() const;
+	unsigned int getLevel() const;
 
-	//Recursively gets all items in this node and in all child nodes.
-	vector<T_ptr>&& getItems() const;
+	void setData(unsigned char newData);
 
-	vector<T_ptr>&& query(float x, float y);
-	bool contains(int x, int y) const;
+	void subdivide(std::function<bool(Quadtree* node)> &predicate);
 
-	//Subdivides this node and all child nodes upon a given predicate.
-	void subdivide(std::function<bool(Quadtree* node)> predicate);
-
-	sf::IntRect getBounds() const;
-
-#ifdef _DEBUG
-	void draw(sf::RenderTarget &target);
-#endif	//_DEBUG
+	void appendToStringStream(std::stringstream &ss);
 };
 
-template <class T>
-Quadtree<T>::Quadtree(int left, int top, int width, int height) :
-mIsLeaf(true),
-mItems(),
-mBounds(left, top, width, height)
+inline Quadtree::Quadtree(float x, float y, float width, float height, Quadtree* parent) :
+mBounds(x, y, width, height),
+mData(),
+mParent( parent )
 {
-	mNodes.reserve(NODE_COUNT);
-#ifdef _DEBUG
-	mShape = sf::RectangleShape(sf::Vector2f(mBounds.width, mBounds.height));
-	mShape.setPosition(mBounds.left, mBounds.top);
-	mShape.setFillColor(sf::Color::Transparent);
-	mShape.setOutlineColor(sf::Color::Magenta);
-	mShape.setOutlineThickness(1.f);
-#endif	//_DEBUG
-}
+	NW = NE = SW = SE = nullptr;
 
-template <class T>
-Quadtree<T>::Quadtree(sf::IntRect const &bounds) :
-mIsLeaf(true),
-mItems(),
-mBounds(bounds)
-{
-	mNodes.reserve(NODE_COUNT);
-#ifdef _DEBUG
-	mShape = sf::RectangleShape(sf::Vector2f(mBounds.width, mBounds.height));
-	mShape.setPosition(mBounds.left, mBounds.top);
-	mShape.setFillColor(sf::Color::Transparent);
-	mShape.setOutlineColor(sf::Color::Magenta);
-	mShape.setOutlineThickness(1.f);
-#endif	//_DEBUG
-}
-
-template <class T>
-Quadtree<T>::~Quadtree() {
-}
-
-template <class T>
-void Quadtree<T>::insert(T_ptr item) {
-	if (mIsLeaf) {
-		mItems.push_back(item);
+	if (parent) {	//if we have a parent, our level is one more than theirs
+		mLevel = parent->getLevel() + 1;
 	} else {
-		//give item to a child node
+		mLevel = 0;	//we are the root node
 	}
 }
 
-template <class T>
-void Quadtree<T>::clear() {
-	mItems.clear();
+inline Quadtree::~Quadtree() {
+	//delete child nodes
+	if (NW) delete NW;
+	if (NE) delete NE;
+	if (SW) delete SW;
+	if (SE) delete SE;
+}
 
-	if (!mIsLeaf) {
-		for (Node n : mNodes) {
-			n->clear();
-		}
+inline unsigned char Quadtree::getData() const {
+	return mData;
+}
+
+inline unsigned Quadtree::getLevel() const {
+	return mLevel;
+}
+
+inline void Quadtree::setData(unsigned char newData) {
+	mData = newData;
+}
+
+//EXAMPLE PREDICATE
+//	unsigned char nodeData = node->getData();
+//	if((nodeData & TerrainData.Grass) && (nodeData & TerrainData.Path)) {
+//		return true;
+//	}
+inline void Quadtree::subdivide(std::function<bool(Quadtree* node)> &predicate) {
+	_ASSERT(NW == nullptr && NE == nullptr && SW == nullptr && SE == nullptr);	//children should be null
+
+	if (predicate(this)) {
+		float halfWidth = mBounds.width / 2.f;
+		float halfHeight = mBounds.height / 2.f;
+
+		//Create child nodes (setting parent to be this)
+		NW = new Quadtree(mBounds.left, mBounds.top, halfWidth, halfHeight, this);
+		NE = new Quadtree(mBounds.left + halfWidth, mBounds.top, halfWidth, halfHeight, this);
+		SW = new Quadtree(mBounds.left, mBounds.top + halfHeight, halfWidth, halfHeight, this);
+		SE = new Quadtree(mBounds.left + halfWidth, mBounds.top + halfHeight, halfWidth, halfHeight, this);
+
+		NW->setData(mData);
+		NE->setData(mData);
+		SW->setData(mData);
+		SE->setData(mData);
+
+		NW->subdivide(predicate);
+		NE->subdivide(predicate);
+		SW->subdivide(predicate);
+		SE->subdivide(predicate);
 	}
 }
 
-template <class T>
-vector<typename Quadtree<T>::T_ptr>&& Quadtree<T>::getItems() const {
-	vector<T_ptr> returnedItems = vector<T_ptr>(mItems);
-
-	for (Node n : mNodes) {
-		returnedItems.emplace_back(n->getItems());
+inline void Quadtree::appendToStringStream(std::stringstream& ss) {
+	if (mLevel == 0) {
+		ss << "QuadTree {\n";
+	}
+	for (int i = 0; i < mLevel; ++i) {
+		ss << "\t";
 	}
 
-	return std::move(returnedItems);
-}
+	ss << "~ " << (mData & 0x01 ? "G" : "") << (mData & 0x02 ? "P" : "") << '\n';
 
-template <class T>
-vector<typename Quadtree<T>::T_ptr>&& Quadtree<T>::query(float x, float y) {
-	_ASSERT(mBounds.contains(x, y));
-
-	vector<T*> returnedItems(mItems);
-
-	for (Node n : mNodes) {
-		if (n->contains(x, y)) {
-			returnedItems.push_back(n->query(x, y));
-			break;	//Breaking because only one of the child nodes will contain the x,y point
-		}
+	if (NW) {
+		NW->appendToStringStream(ss);
+		NE->appendToStringStream(ss);
+		SW->appendToStringStream(ss);
+		NE->appendToStringStream(ss);
 	}
-
-	return std::move(returnedItems);
-}
-
-template <class T>
-bool Quadtree<T>::contains(int x, int y) const {
-	return mBounds.contains(x, y);
-}
-
-template <class T>
-bool Quadtree<T>::contains(const T_ptr item, bool* whollyContained) const {
-	bool isContained = false;
-
-	if (mBounds.intersects(item->getBounds())) {
-		isContained = true;
-
-		if (whollyContained != nullptr) {
-			auto itemBounds = item->getBounds();
-			*whollyContained = (
-				itemBounds.left >= mBounds.left &&
-				itemBounds.top >= mBounds.top &&
-				itemBounds.left + itemBounds.width <= mBounds.left + mBounds.width &&
-				itemBounds.top + itemBounds.height <= mBounds.top + mBounds.height
-				);
-		}//end if(!nullptr)
-
-	}//end if(intersects)
-
-	return isContained;
-}
-
-template <class T>
-void Quadtree<T>::subdivide(std::function<bool(Quadtree* node)> predicate) {
-
-	if (mIsLeaf && predicate(this)) {
-		mIsLeaf = false;
-
-		//Create child nodes
-		unsigned int halfWidth = mBounds.width / 2;
-		unsigned int halfHeight = mBounds.height / 2;
-		mNodes[NW] = new Quadtree<T>(mBounds.left, mBounds.top, halfWidth, halfHeight);
-		mNodes[NE] = new Quadtree<T>(mBounds.left + halfWidth, mBounds.top, halfWidth, halfHeight);
-		mNodes[SW] = new Quadtree<T>(mBounds.left, mBounds.top + halfHeight, halfWidth, halfHeight);
-		mNodes[SE] = new Quadtree<T>(mBounds.left + halfWidth, mBounds.top + halfHeight, halfWidth, halfHeight);
-
-		for (T_ptr item : mItems) {
-			for (Node n : mNodes) {
-				
-				//Move items to child nodes (only if wholly contained??).
-				//(if we give this node the item, break so that we don't try to give it to another node)
-
-			}//end for(each node)
-		}//end for(each item)
-	}//end if(leaf && predicate)
-}
-
-template <class T>
-sf::IntRect Quadtree<T>::getBounds() const {
-	return mBounds;
-}
-
-#ifdef _DEBUG
-template <class T>
-void Quadtree<T>::draw(sf::RenderTarget& target) {
-	if (mIsLeaf) {
-		target.draw(mShape);
-	} else {
-		for (Node n : mNodes) {
-			n->draw(target);
-		}
+	if (mLevel == 0) {
+		ss << "}";
 	}
 }
-#endif	//_DEBUG
 #endif	//include guard
--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,37 @@
 
 
 #include <include/Game.hpp>
+#include <include/Quadtree.hpp>
+
+unsigned char GRASS = 0x01;
+unsigned char PATH = 0x02;
+unsigned int maxLevel = 9;
+
+bool myPredicate(Quadtree* node) {
+	bool subdivide = false;
+
+	if (node->getLevel() > 2) {
+		node->setData(PATH);
+	}
+
+	unsigned char nodeData = node->getData();
+	if ((nodeData & GRASS) && (nodeData & PATH) && node->getLevel() < maxLevel) {
+		subdivide = true;
+	}
+
+	return subdivide;
+}
 
 int main() {
+
+	Quadtree myTree(0.f, 0.f, 1000.f, 1000.f);
+	myTree.setData(GRASS | PATH);
+	std::function<bool(Quadtree*)> f = &myPredicate;
+	myTree.subdivide(f);
+	std::stringstream ss = std::stringstream();
+	myTree.appendToStringStream(ss);
+	std::cout << ss.str() << std::endl;
+	std::cin.get();
 	
 	Game game;
 	return game.run();
