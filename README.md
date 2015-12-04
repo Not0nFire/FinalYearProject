0fa83e8301dfb5653ed6c8929ba97c44277ccdd0
Alex Meuer <alexmeuer@gmail.com>
2015-12-04 11:25:13 +0000

    Initial gh-pages commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# FinalYearProject
-Tower defence game involving moba elements.
--- src/Minion.cpp
+++
@@ -1,8 +0,0 @@
-#include <include/Minion.hpp>
-
-Minion::Minion(sf::Texture& texture, Faction _faction) : Pawn(texture), mFaction(_faction) {
-}
-
-Minion::~Minion() {
-	
-}
